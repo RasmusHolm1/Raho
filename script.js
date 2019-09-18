8caff475c3d35f516f4e1e248f0bb1d9dfa385ec
RasmusHolm1 <rasmus.holm1@gmail.com>
2019-09-18 21:03:18 +0200

    Final

--- script.js
+++ script.js
@@ -48,15 +48,11 @@ function toggleBoxes() {
 
     const circleRightIsExpanded = circleRightEl.classList.contains("changingcircle--expanded")
 
-    //Om vi just nu visar den "korrekta" cirkeln
+
     if (circleRightIsExpanded) {
         circleRightEl.classList.remove("changingcircle--expanded");
         circleWrongEl.classList.add("changingcircle--expanded");
-        // Tänk på att bevara övriga klasser
-        //Dölja den korrekta cirkeln
-        //Ta bort klassen changingcircle--expanded på rightEl
-        //Visa den inkorrekta
-        //Lägg till klassen changingcircle--expanded på wrongEl
+
     } else {
         circleRightEl.classList.add("changingcircle--expanded");
         circleWrongEl.classList.remove("changingcircle--expanded");
